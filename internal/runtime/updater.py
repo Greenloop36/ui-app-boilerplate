f3abdaac6279d280104ac244cc761ff5735f60ca
gl36 <ewanakira@gmail.com>
2025-06-20 10:27:58 +0100

    Add interface to updater

--- internal/runtime/updater.py
+++ internal/runtime/updater.py
@@ -28,19 +28,65 @@ def format_error(exception: Exception, message: str = None) -> str:
         message = ""
     return f'{message}[{name}]: {str(exception)}'
 
+class _UpdaterUI:
+    def __init__(self, title: str, ui_master: tk.Tk | None = None) -> None:
+        self.ui_master = ui_master
+        self.title = title
+
+        if not self.ui_master:
+            self.root = tk.Tk()
+        else:
+            self.root = tk.Toplevel(self.ui_master)
+        
+        # Create UI
+        self.container = ttk.Frame(self.root, padding=10)
+        self.container.grid()
+
+        self.label_title = ttk.Label(self.container, text=self.title, anchor="w")
+        self.label_title.pack(expand=True, fill="x")
+
+        self.label_status = ttk.Label(self.container, text="waiting to update...", anchor="w")
+        self.label_status.pack(expand=True, fill="x")
+
+        self.progress_bar = ttk.Progressbar(self.container, length=300)
+        self.progress_bar.pack(fill="x", expand=True, pady=(20, 0))
+    
+    def set_status(self, text: str = ""):
+        self.label_status.config(text=text)
+        self.root.update()
+    
+    def set_progress(self, current: float | int = None, max: float| int = None):
+        if current != None:
+            self.progress_bar["value"] = current
+        
+        if max != None:
+            self.progress_bar.config(maximum=max)
+        
+        self.root.update()
+
 class Updater:
-    def __init__(self, url: str, directory: str, branch: str = "main") -> None:
+    def __init__(self, url: str, directory: str, branch: str = "main", update_name: str = "performing software update...", ui_master: tk.Tk = None) -> None:
         self.url = url
         self.dir = directory
         self.branch = branch
+        self.ui_master = ui_master
+        self.title = update_name
     
     def update(self) -> tuple[bool, str | None]:
-        # Download the archive of
+        # Create UI
+        interface = _UpdaterUI(self.title)
+        interface.set_progress(0, 5)
+       
+
+        # Download new archive
+        interface.set_status("downloading archive")
         try:
             download_response = requests.get(f'{self.url}/{self.branch}')
             download_response.raise_for_status()
         except Exception as e:
             return False, format_error(e, "Download failed!")
+        
+        interface.set_progress(1)
         
         # Install the downloaded archive in a temporary file
         try:
@@ -50,6 +96,7 @@ class Updater:
             return False, format_error(e, "Failed to install temporary archive!")
         
         # Extract the downloaded archive
+        interface.set_status("extracting downloaded archive")
         try:
             # Create a temporary directory to extract to
             archive_dump = tempfile.TemporaryDirectory(ignore_cleanup_errors=True)
@@ -64,20 +111,31 @@ class Updater:
         except Exception as e:
             return False, format_error(e, "Failed to extract the downloaded archive!")
         
+        interface.set_progress(2)
+        
         # Remove the current files
+        interface.set_status("removing current installation")
         try:
             shutil.rmtree(self.dir)
             os.makedirs(self.dir)
         except Exception as e:
             return False, format_error(e, "Failed to remove the current installation!")
+        
+        interface.set_progress(3)
 
         # Install the extracted data
+        interface.set_status("installing update")
         try:
             shutil.copytree(archive_dump.name, self.dir, dirs_exist_ok=True)
         except Exception as e:
             return False, format_error(e, "Failed to install new files!")
         
+        interface.set_progress(4)
+        
         # Finish the update
+        interface.set_status("finishing software update")
         archive_dump.cleanup()
+
+        interface.root.destroy()
 
         return True, None
